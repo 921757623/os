14272ee1f6571bbb36cb66b3322a4e4328ee598b
921757623 <www.921757623@qq.com>
2024-03-02 02:33:54 +0000

    wait

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -16,7 +16,8 @@ process user_app;
 // load the elf, and construct a "process" (with only a trapframe).
 // load_bincode_from_host_elf is defined in elf.c
 //
-void load_user_program(process *proc) {
+void load_user_program(process *proc)
+{
   // USER_TRAP_FRAME is a physical address defined in kernel/config.h
   proc->trapframe = (trapframe *)USER_TRAP_FRAME;
   memset(proc->trapframe, 0, sizeof(trapframe));
@@ -31,19 +32,21 @@ void load_user_program(process *proc) {
 //
 // s_start: S-mode entry point of riscv-pke OS kernel.
 //
-int s_start(void) {
-  sprint("hartid = ?: Enter supervisor mode...\n");
+int s_start(void)
+{
+  uint64 hartid = read_tp();
+  sprint("hartid = %d: Enter supervisor mode...\n", hartid);
   // Note: we use direct (i.e., Bare mode) for memory mapping in lab1.
   // which means: Virtual Address = Physical Address
   // therefore, we need to set satp to be 0 for now. we will enable paging in lab2_x.
-  // 
+  //
   // write_csr is a macro defined in kernel/riscv.h
   write_csr(satp, 0);
 
   // the application code (elf) is first loaded into memory, and then put into execution
   load_user_program(&user_app);
 
-  sprint("hartid = ?: Switch to user mode...\n");
+  sprint("hartid = %d: Switch to user mode...\n", hartid);
   // switch_to() is defined in kernel/process.c
   switch_to(&user_app);
 
